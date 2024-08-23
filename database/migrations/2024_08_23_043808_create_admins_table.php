59adf754e93ed3b5ca82c9f4c3aa7b1b36378911
Okty Aji Pangestu <oktyajipangestu@gamil.com>
2024-08-23 11:44:40 +0700

    initial comit

---
+++ database/migrations/2024_08_23_043808_create_admins_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('admins', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('email', 100);
+            $table->text('password');
+            $table->string('role', 20)->default('ADMIN');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('admins');
+    }
+};
